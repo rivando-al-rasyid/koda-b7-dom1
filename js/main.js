7344050d4a3ee6b738cd3191bf2f3ab55b16db78
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-17 11:59:36 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,14 @@ const suhu = inputsuhu;
 const  hasilF = (suhu * 9/5) + 32;
 // const  hasilF = (suhu * 9/5) + 32;
 const output = document.getElementById('hasil')
-output.textContent = "suhu " + hasilF;
+output.textContent = "Farenheit  " + hasilF;
+}
+
+function farnheitToCelcius() {
+const  inputsuhu = document.getElementById("suhu").value
+const suhu = inputsuhu;
+const  hasilF = (suhu - 32) * (5/9);
+// const  hasilF = (suhu * 9/5) + 32;
+const output = document.getElementById('hasil')
+output.textContent = "Celcius " + hasilF;
 }
